72d6dc9986e65beabfb56434b17034ed3ba94dc0
Nandini <nandinisain@yahoo.in>
2025-02-25 17:15:44 +0530

    Configured GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"), // This allows "@/components/ui" to work
-    },
-  },
+  base: "/Web-Content-AQ-Tool/",  // Set this to your repo name
 });
