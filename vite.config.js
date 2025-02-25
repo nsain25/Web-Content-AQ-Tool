8ddc0a930bf38a3ea5cb8cae2ebf884c016c7389
Nandini <nandinisain@yahoo.in>
2025-02-25 17:57:09 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Web-Content-AQ-Tool/', // Ensure this matches your GitHub repo name
+  base: '/Web-Content-AQ-Tool/', // Ensure this matches your repo name EXACTLY
 });
