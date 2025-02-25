fd0cbe40cbfa6623e09b0da14126088491bc2271
Nandini <nandinisain@yahoo.in>
2025-02-25 18:08:35 +0530

    Fixed index.html script tag issue

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Web-Content-AQ-Tool/', // Ensure it matches EXACTLY with your repo name
+  base: '/Web-Content-AQ-Tool/', // Ensure this matches your repo name exactly
+  build: {
+    outDir: 'dist',
+  },
 });
